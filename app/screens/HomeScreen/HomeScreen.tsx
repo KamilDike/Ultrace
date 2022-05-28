12ea3901038a5597573ec16b0dc889be5c5bd4c7
KD <kamil.dike.96@gmail.com>
2022-05-29 01:56:13 +0200

    feat: HomeScreen component

--- app/screens/HomeScreen/HomeScreen.tsx
+++ app/screens/HomeScreen/HomeScreen.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {FlatList, SafeAreaView, View} from 'react-native';
+import Post from '../../components/Post/Post';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import {ContainerStyles} from '../../styles/ContainerStyles';
+import {HomeScreenStyles} from './HomeScreenStyles';
+import UserPicture from '../../components/UserPicture/UserPicture';
+
+const mockUris = [
+  {
+    uri: 'https://static.carthrottle.com/workspace/uploads/posts/2017/12/3fc8e7f25dd9d7e79b5accb90809bc9a.jpg'
+  },
+  {
+    uri: 'https://static.carthrottle.com/workspace/uploads/posts/2017/12/3fc8e7f25dd9d7e79b5accb90809bc9a.jpg'
+  },
+  {
+    uri: 'https://static.carthrottle.com/workspace/uploads/posts/2017/12/3fc8e7f25dd9d7e79b5accb90809bc9a.jpg'
+  },
+  {
+    uri: 'https://static.carthrottle.com/workspace/uploads/posts/2017/12/3fc8e7f25dd9d7e79b5accb90809bc9a.jpg'
+  }
+];
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <SafeAreaView style={ContainerStyles.center}>
+      <View style={HomeScreenStyles.header}>
+        <SearchBar />
+        <UserPicture size={30} />
+      </View>
+      <FlatList
+        data={mockUris}
+        style={HomeScreenStyles.flatList}
+        renderItem={({item}) => <Post uri={item.uri} />}
+      />
+    </SafeAreaView>
   );
 };
 
---
+++ app/screens/HomeScreen/HomeScreenStyles.ts
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+export const HomeScreenStyles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    width: '100%',
+    height: 40,
+    paddingHorizontal: 10,
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  flatList: {
+    width: '100%'
+  }
+});
