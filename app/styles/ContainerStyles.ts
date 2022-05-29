581a3711fe01a276db2de16bfd0fdd04e044e7bb
KD <kamil.dike.96@gmail.com>
2022-05-29 17:31:54 +0200

    feat: reusable shadow

--- app/styles/ContainerStyles.ts
+++ app/styles/ContainerStyles.ts
@@ -3,7 +3,16 @@ import {StyleSheet} from 'react-native';
 export const ContainerStyles = StyleSheet.create({
   center: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: 'center'
+  },
+  basicShadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
   }
 });
